a661a9ff252e331c7272ef3b3861e8c4658d3982
a7med3oda <a7med3oda@yahoo.com>
2022-12-13 06:46:13 +0200

    Create Controllers

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly EcommerceDbContext _context;                
+        public CategoryController(EcommerceDbContext context)       // CTOR Dependency injection
+        {
+            _context = context;
+        }
+
+        public async task<IActionResult> Index()
+        {
+            var response = await _context.Categories.ToListAsync();
+            return View(response);
+        }
+    }
+}
---
+++ Ecommerce/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        public ProductController(EcommerceDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var response = await _context.Products.ToListAsync();
+            return View(response);
+        }
+    }
+}
